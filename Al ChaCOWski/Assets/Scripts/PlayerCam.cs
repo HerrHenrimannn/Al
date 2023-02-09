ae34a18d03654e60d6614807fcfdd5923a58cd8c
Henri <5555hhh555henri@gmail.com>
2023-02-09 16:50:07 +0100

    .

--- Al ChaCOWski/Assets/Scripts/PlayerCam.cs
+++ Al ChaCOWski/Assets/Scripts/PlayerCam.cs
@@ -11,7 +11,7 @@ public class PlayerCam : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible= false;
+        Cursor.visible = false;
     }
 
     private void Update()
